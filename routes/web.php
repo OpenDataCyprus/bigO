45d2cbb3236aab27ca7b143eaa407505552aabac
Lefteris Kameris <lefteris.k@3elalliance.com>
2016-09-11 01:50:41 +0300

    Fixing Error

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,6 @@ Route::get('/auth/github/callback','AuthController@handleProviderCallback');
 Route::get('/','HomeController@index');
 Auth::routes();
 
-<<<<<<< HEAD
-Route::get('/home', 'HomeController@index');
 Route::get('/details', 'HomeController@details');
-=======
+
 Route::get('/home', 'DashboardController@index');
->>>>>>> feature/auth
