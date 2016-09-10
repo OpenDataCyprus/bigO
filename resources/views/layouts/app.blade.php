545db8944fd59098e6f88fd36031e8faec73d650
Nicos Mouzouras <mouzourasn@gmail.com>
2016-09-10 19:45:39 +0300

    Font Awesome

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,12 +12,10 @@
 
     <!-- Styles -->
     <link href="/css/app.css" rel="stylesheet">
-<<<<<<< Updated upstream
-=======
+
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
     
     @stack('styles')
->>>>>>> Stashed changes
     <!-- Scripts -->
     <script>
         window.Laravel = <?php echo json_encode([
