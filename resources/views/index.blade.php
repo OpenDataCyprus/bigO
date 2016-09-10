97275af355b2ca1f9888df472f5f41421936b9f4
Lefteris Kameris <lefteris.k@3elalliance.com>
2016-09-10 14:56:24 +0300

    Setting up Nick

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-offset-3 col-md-6">
+                This is a test
+            </div>
+        </div>
+    </div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+Route::get('/','HomeController@index');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
